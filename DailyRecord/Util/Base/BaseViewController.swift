75626572c114cde129c0c536839a2fcc859ee854
sunghun <leedool3003@gmail.com>
2024-06-09 16:16:29 +0900

    :hammer: [chore] BaseViewController, BaseViewModel 세팅 #6

---
+++ DailyRecord/Util/Base/BaseViewController.swift
@@ -0,0 +1,24 @@
+//
+//  BaseViewController.swift
+//  DailyRecord
+//
+//  Created by Kim SungHun on 6/9/24.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+	override func viewDidLoad() {
+			super.viewDidLoad()
+		
+			addView()
+			setupView()
+			setLayout()
+	}
+	
+	func addView() { }
+	
+	func setupView() { }
+	
+	func setLayout() { }
+}
