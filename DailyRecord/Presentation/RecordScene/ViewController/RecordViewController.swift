0fd51b3855574cf58c4841eb0a90ae9ea8ab7642
sunghun <leedool3003@gmail.com>
2024-06-16 14:17:53 +0900

    :recycle: [refactor] Record 코드 정리 #6

--- DailyRecord/Presentation/RecordScene/ViewController/RecordViewController.swift
+++ DailyRecord/Presentation/RecordScene/ViewController/RecordViewController.swift
@@ -15,9 +15,54 @@ final class RecordViewController: BaseViewController {
 	
 	var coordinator: RecordCoordinator?
 	
+	private let viewModel: RecordViewModel
+	
 	// MARK: - Views
 	
-	private let popupView: UIView = {
+	private let scrollView: UIScrollView = {
+		let scrollView = UIScrollView()
+		return scrollView
+	}()
+	
+	private let contentView: UIView = {
+		let view = UIView()
+		return view
+	}()
+	
+	private let todayEmotionImageView: UIImageView = {
+		let imageView = UIImageView()
+		imageView.backgroundColor = .yellow
+		imageView.isUserInteractionEnabled = true
+		return imageView
+	}()
+	
+	private let todayDateView: UILabel = {
+		let label = UILabel()
+		label.textAlignment = .center
+		return label
+	}()
+	
+	private lazy var inputDiaryView: UITextView = {
+		let textView = UITextView()
+		textView.text = "문구 입력..."
+		textView.textColor = .secondaryLabel
+		textView.font = .systemFont(ofSize: 15.0)
+		textView.delegate = self
+		textView.backgroundColor = .green
+		textView.isScrollEnabled = false
+		return textView
+	}()
+	
+	private let divider: UIView = {
+		let view = UIView()
+		view.backgroundColor = .red
+		return view
+	}()
+	
+	// TODO: Custom View로 분리
+	private var dimmingView: UIView?
+	
+	private let emotionalImagePopupView: UIView = {
 		let view = UIView()
 		view.backgroundColor = .white
 		view.layer.cornerRadius = 10
@@ -28,103 +73,77 @@ final class RecordViewController: BaseViewController {
 		return view
 	}()
 	
-	private let closeButton: UIButton = {
-		let button = UIButton(type: .system)
-		button.setTitle("Close", for: .normal)
-		button.addTarget(self, action: #selector(closePopup), for: .touchUpInside)
-		return button
-	}()
-	
-	// TODO: 감정표현 ImageView
-	private let todayEmotionImageView: UIImageView = {
-		let imageView = UIImageView()
-		imageView.backgroundColor = .yellow
-		imageView.isUserInteractionEnabled = true
-		return imageView
-	}()
-	
-	// TODO: 오늘 날짜
-	private let todayDateView: UILabel = {
-		let label = UILabel()
-		label.text = "오늘 날짜"
-		label.textAlignment = .center
-		return label
-	}()
-	
-	// TODO: 일기 택스트 필드
-	private let inputDiaryView: UIView = {
+	// TODO: 감정 이미지로 수정
+	private let demoImage1: UIView = {
 		let view = UIView()
-		view.backgroundColor = .lightGray
+		view.backgroundColor = .blue
+		return view
+	}()
+	private let demoImage2: UIView = {
+		let view = UIView()
+		view.backgroundColor = .red
+		return view
+	}()
+	private let demoImage3: UIView = {
+		let view = UIView()
+		view.backgroundColor = .green
+		return view
+	}()
+	private let demoImage4: UIView = {
+		let view = UIView()
+		view.backgroundColor = .purple
+		return view
+	}()
+	private let demoImage5: UIView = {
+		let view = UIView()
+		view.backgroundColor = .brown
 		return view
 	}()
 	
-	// TODO: 저장 버튼
-	
 	// MARK: - Life Cycle
+	
+	init(
+		viewModel: RecordViewModel
+	) {
+		self.viewModel = viewModel
+		super.init(nibName: nil, bundle: nil)
+	}
+	
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
 		// 작성이 안 되어 있으면 화면 가운데 alert 올라오게
-		
-		view.backgroundColor = .white
-		
-		let tapGesture = UITapGestureRecognizer(target: self, action: #selector(showPopup))
-		todayEmotionImageView.addGestureRecognizer(tapGesture)
 	}
-	
-	private var dimmingView: UIView?
-	
-	
-	@objc func showPopup() {
-		dimmingView = UIView(frame: view.bounds)
-		dimmingView?.backgroundColor = UIColor.black.withAlphaComponent(0.5)
-		view.addSubview(dimmingView!)
-		view.addSubview(popupView)
-		popupView.addSubview(closeButton)
-		
-		popupView.snp.makeConstraints { make in
-			make.center.equalToSuperview()
-			make.width.equalTo(250)
-			make.height.equalTo(150)
-		}
-		
-		closeButton.snp.makeConstraints { make in
-			make.centerX.equalToSuperview()
-			make.bottom.equalToSuperview().offset(-20)
-		}
-		
-		// 팝업뷰 애니메이션 (선택사항)
-		popupView.alpha = 0
-		dimmingView?.alpha = 0
-		UIView.animate(withDuration: 0.3) {
-			self.popupView.alpha = 1
-			self.dimmingView?.alpha = 1
-		}
-	}
-	
-	@objc func closePopup() {
-		UIView.animate(withDuration: 0.3, animations: {
-			self.popupView.alpha = 0
-			self.dimmingView?.alpha = 0
-		}) { _ in
-			self.popupView.removeFromSuperview()
-			self.dimmingView?.removeFromSuperview()
-		}
-	}
-	
 	
 	// MARK: - Functions
 	
 	override func addView() {
-		[todayEmotionImageView, todayDateView, inputDiaryView].forEach {
+		[scrollView, divider].forEach {
 			view.addSubview($0)
+		}
+		
+		scrollView.addSubview(contentView)
+		[todayEmotionImageView, todayDateView, inputDiaryView].forEach {
+			contentView.addSubview($0)
 		}
 	}
 	
 	override func setLayout() {
+		scrollView.snp.makeConstraints { make in
+			make.edges.equalToSuperview()
+		}
+		
+		contentView.snp.makeConstraints { make in
+			make.edges.equalTo(scrollView.contentLayoutGuide)
+			make.width.equalTo(scrollView.frameLayoutGuide)
+		}
+		
 		todayEmotionImageView.snp.makeConstraints { make in
-			make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(20)
+			make.top.equalTo(contentView.snp.top).offset(20)
 			make.centerX.equalToSuperview()
 			make.width.height.equalTo(100)
 		}
@@ -137,11 +156,102 @@ final class RecordViewController: BaseViewController {
 		inputDiaryView.snp.makeConstraints { make in
 			make.top.equalTo(todayDateView.snp.bottom).offset(20)
 			make.left.right.equalToSuperview().inset(20)
-			make.height.equalTo(200)
+			make.bottom.equalToSuperview()
+		}
+		
+		divider.snp.makeConstraints { make in
+			make.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom)
+			make.left.right.equalToSuperview()
+			make.height.equalTo(2)
 		}
 	}
 	
 	override func setupView() {
+		view.backgroundColor = .white
 		
+		todayDateView.text = String(describing: viewModel.selectDate)
+		
+		let tapGesture = UITapGestureRecognizer(target: self,
+																						action: #selector(showPopup))
+		todayEmotionImageView.addGestureRecognizer(tapGesture)
+	}
+}
+
+private extension RecordViewController {
+	@objc func showPopup() {
+		dimmingView = UIView(frame: view.bounds)
+		dimmingView?.backgroundColor = UIColor.black.withAlphaComponent(0.5)
+		let tapGesture = UITapGestureRecognizer(target: self, action: #selector(closePopup))
+		dimmingView?.addGestureRecognizer(tapGesture)
+		
+		view.addSubview(dimmingView!)
+		view.addSubview(emotionalImagePopupView)
+		
+		emotionalImagePopupView.addSubview(demoImage1)
+		emotionalImagePopupView.addSubview(demoImage2)
+		emotionalImagePopupView.addSubview(demoImage3)
+		emotionalImagePopupView.addSubview(demoImage4)
+		emotionalImagePopupView.addSubview(demoImage5)
+		
+		demoImage1.snp.makeConstraints { make in
+			make.top.equalToSuperview().offset(20)
+			make.left.equalToSuperview().offset(20)
+			make.width.height.equalTo(60)
+		}
+		
+		demoImage2.snp.makeConstraints { make in
+			make.top.equalToSuperview().offset(20)
+			make.centerX.equalToSuperview()
+			make.width.height.equalTo(60)
+		}
+		
+		demoImage3.snp.makeConstraints { make in
+			make.top.equalToSuperview().offset(20)
+			make.right.equalToSuperview().offset(-20)
+			make.width.height.equalTo(60)
+		}
+		
+		demoImage4.snp.makeConstraints { make in
+			make.top.equalTo(demoImage1.snp.bottom).offset(20)
+			make.centerX.equalToSuperview().offset(-50)
+			make.width.height.equalTo(60)
+		}
+		
+		demoImage5.snp.makeConstraints { make in
+			make.top.equalTo(demoImage3.snp.bottom).offset(20)
+			make.centerX.equalToSuperview().offset(50)
+			make.width.height.equalTo(60)
+		}
+		
+		emotionalImagePopupView.snp.makeConstraints { make in
+			make.center.equalToSuperview()
+			make.width.equalTo(250)
+			make.height.equalTo(180)
+		}
+		
+		emotionalImagePopupView.alpha = 0
+		dimmingView?.alpha = 0
+		UIView.animate(withDuration: 0.3) {
+			self.emotionalImagePopupView.alpha = 1
+			self.dimmingView?.alpha = 1
+		}
+	}
+	
+	@objc func closePopup() {
+		UIView.animate(withDuration: 0.3, animations: {
+			self.emotionalImagePopupView.alpha = 0
+			self.dimmingView?.alpha = 0
+		}) { _ in
+			self.emotionalImagePopupView.removeFromSuperview()
+			self.dimmingView?.removeFromSuperview()
+		}
+	}
+}
+
+extension RecordViewController: UITextViewDelegate {
+	func textViewDidBeginEditing(_ textView: UITextView) {
+		guard textView.textColor == .secondaryLabel else { return }
+		textView.text = nil
+		textView.textColor = .label
 	}
 }
