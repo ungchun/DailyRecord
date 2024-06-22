6fe4edd8e652490bee84d7ebc428ae388831793d
sunghun <leedool3003@gmail.com>
2024-06-22 18:12:22 +0900

    :hammer: [chore] Login DI, Coordinator 세팅 #9

---
+++ DailyRecord/Application/DIContainer/LoginDIContainer.swift
@@ -0,0 +1,42 @@
+//
+//  LoginDIContainer.swift
+//  DailyRecord
+//
+//  Created by Kim SungHun on 6/22/24.
+//
+
+import UIKit
+
+final class LoginDIContainer: DIContainer {
+	private let navigationController: UINavigationController
+	
+	init(navigationController: UINavigationController) {
+		self.navigationController = navigationController
+	}
+}
+
+extension LoginDIContainer {
+	
+	// MARK: - Login
+	
+	func makeLoginCoordinator() -> LoginCoordinator {
+		return LoginCoordinator(DIContainer: self,
+														navigationController: navigationController)
+	}
+	
+	func makeLoginViewController() -> LoginViewController {
+		return LoginViewController(viewModel: makeLoginViewModel())
+	}
+	
+	private func makeLoginViewModel() -> LoginViewModel {
+		return LoginViewModel()
+	}
+	
+	// MARK: - Calender
+	
+	func makeCalenderDIContainer() -> CalenderDIContainer {
+		return CalenderDIContainer(
+			navigationController: navigationController
+		)
+	}
+}
