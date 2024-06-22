09ee9260e73709dd2c19095fa8b4369fc2010dec
sunghun <leedool3003@gmail.com>
2024-06-22 18:13:41 +0900

    :hammer: [chore] scenedelegate에 캘린더, 로그인 뷰 로직 추가 #9

--- DailyRecord/Application/SceneDelegate.swift
+++ DailyRecord/Application/SceneDelegate.swift
@@ -11,23 +11,44 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 	
 	var window: UIWindow?
 	
+	var loginTempValue = false
+	
 	func scene(_ scene: UIScene,
 						 willConnectTo session: UISceneSession,
 						 options connectionOptions: UIScene.ConnectionOptions) {
 		if let windowScene = scene as? UIWindowScene {
-			var calenderDIContainer: CalenderDIContainer?
-			var calenderCoordinator: CalenderCoordinator?
-			let window = UIWindow(windowScene: windowScene)
-			self.window = window
 			
-			let navigationController = UINavigationController()
-			self.window?.rootViewController = navigationController
-			
-			calenderDIContainer = CalenderDIContainer(navigationController: navigationController)
-			calenderCoordinator = calenderDIContainer?.makeCalenderCoordinator()
-			calenderCoordinator?.start()
-			
-			self.window?.makeKeyAndVisible()
+			if loginTempValue {
+				// 캘린더 뷰
+				var calenderDIContainer: CalenderDIContainer?
+				var calenderCoordinator: CalenderCoordinator?
+				let window = UIWindow(windowScene: windowScene)
+				self.window = window
+				
+				let navigationController = UINavigationController()
+				self.window?.rootViewController = navigationController
+				
+				calenderDIContainer = CalenderDIContainer(navigationController: navigationController)
+				calenderCoordinator = calenderDIContainer?.makeCalenderCoordinator()
+				calenderCoordinator?.start()
+				
+				self.window?.makeKeyAndVisible()
+			} else {
+				// 로그인 뷰
+				var loginDIContainer: LoginDIContainer?
+				var loginCoordinator: LoginCoordinator?
+				let window = UIWindow(windowScene: windowScene)
+				self.window = window
+				
+				let navigationController = UINavigationController()
+				self.window?.rootViewController = navigationController
+				
+				loginDIContainer = LoginDIContainer(navigationController: navigationController)
+				loginCoordinator = loginDIContainer?.makeLoginCoordinator()
+				loginCoordinator?.start()
+				
+				self.window?.makeKeyAndVisible()
+			}
 		}
 	}
 	
