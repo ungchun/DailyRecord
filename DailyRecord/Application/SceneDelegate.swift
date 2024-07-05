06919fa2b52f2a320e6d0b384682ace67581c0fb
sunghun <leedool3003@gmail.com>
2024-07-06 00:00:06 +0900

    :hammer: [chore] 자동 로그인 로직 #13

--- DailyRecord/Application/SceneDelegate.swift
+++ DailyRecord/Application/SceneDelegate.swift
@@ -18,14 +18,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 						 willConnectTo session: UISceneSession,
 						 options connectionOptions: UIScene.ConnectionOptions) {
 		if let windowScene = scene as? UIWindowScene {
-			
-			/**
-			 # 자동 로그인 로직 구현하기
-			 Auth.auth().currentUser == nil : auth 등록이 안 되어 있거나
-			 싱글턴 docID 값이 없을 때 : 가입은 되어 있으나 앱을 다시 깔았을 경우
-			 */
-			
-			if Auth.auth().currentUser == nil {
+			if Auth.auth().currentUser == nil || UserDefaultsSetting.uid.isEmpty {
 				// 로그인 뷰
 				var loginDIContainer: LoginDIContainer?
 				var loginCoordinator: LoginCoordinator?
