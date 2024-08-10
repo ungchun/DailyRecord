425643d104b88e5f690aea0560b1ceff7940b9f6
sunghun <leedool3003@gmail.com>
2024-08-10 20:00:30 +0900

    :hammer: [chore] end date 값 변경 #22

--- DailyRecord/Data/Repository/CalendarRepository.swift
+++ DailyRecord/Data/Repository/CalendarRepository.swift
@@ -14,65 +14,65 @@ final class CalendarRepository: DefaultCalendarRepository {
 
 extension CalendarRepository {
 	func readMonthRecord(year: Int, month: Int) async throws -> [RecordResponseDTO] {
-			guard let userID = Auth.auth().currentUser?.uid else {
-					throw NSError(domain: "AuthError",
-												code: 1001,
-												userInfo: [NSLocalizedDescriptionKey: "User not authenticated"])
-			}
-			
-			let db = Firestore.firestore()
-			
-			// 월의 시작과 끝을 설정
-			var components = DateComponents()
-			components.year = year
-			components.month = month
-			components.day = 1
-			let calendar = Calendar.current
-			
-			guard let startOfMonth = calendar.date(from: components) else {
-					throw NSError(domain: "DateError",
-												code: 1002,
-												userInfo: [NSLocalizedDescriptionKey: "Invalid start of month date"])
-			}
-			
-			components.month = 7 + 1
-			components.day = 0
-			guard let endOfMonth = calendar.date(from: components) else {
-					throw NSError(domain: "DateError",
-												code: 1003,
-												userInfo: [NSLocalizedDescriptionKey: "Invalid end of month date"])
-			}
-			
-			let startTimestamp = Int(startOfMonth.timeIntervalSince1970 * 1000)
-			let endTimestamp = Int(endOfMonth.timeIntervalSince1970 * 1000)
-			
-			let documentRef = db.collection("user").document(userID).collection("record")
-			
-			let query = documentRef
-					.whereField("calendar_date", isGreaterThanOrEqualTo: startTimestamp)
-					.whereField("calendar_date", isLessThanOrEqualTo: endTimestamp)
-			
-			return try await withCheckedThrowingContinuation { continuation in
-					query.getDocuments { (querySnapshot, error) in
-							if let error = error {
-									continuation.resume(throwing: error)
-							} else {
-									do {
-											var records: [RecordResponseDTO] = []
-											for document in querySnapshot!.documents {
-													let data = document.data()
-													let jsonData = try JSONSerialization.data(withJSONObject: data,
-																																		options: [])
-													let recordResponse = try JSONDecoder().decode(RecordResponseDTO.self,
-																																				from: jsonData)
-													records.append(recordResponse)
-											}
-											continuation.resume(returning: records)
-									} catch {
-											continuation.resume(throwing: error)
-									}
-							}
+		guard let userID = Auth.auth().currentUser?.uid else {
+			throw NSError(domain: "AuthError",
+										code: 1001,
+										userInfo: [NSLocalizedDescriptionKey: "User not authenticated"])
+		}
+		
+		let db = Firestore.firestore()
+		
+		// 월의 시작과 끝을 설정
+		var components = DateComponents()
+		components.year = year
+		components.month = month
+		components.day = 1
+		let calendar = Calendar.current
+		
+		guard let startOfMonth = calendar.date(from: components) else {
+			throw NSError(domain: "DateError",
+										code: 1002,
+										userInfo: [NSLocalizedDescriptionKey: "Invalid start of month date"])
+		}
+		
+		components.month = month + 1
+		components.day = 0
+		guard let endOfMonth = calendar.date(from: components) else {
+			throw NSError(domain: "DateError",
+										code: 1003,
+										userInfo: [NSLocalizedDescriptionKey: "Invalid end of month date"])
+		}
+		
+		let startTimestamp = Int(startOfMonth.timeIntervalSince1970 * 1000)
+		let endTimestamp = Int(endOfMonth.timeIntervalSince1970 * 1000)
+		
+		let documentRef = db.collection("user").document(userID).collection("record")
+		
+		let query = documentRef
+			.whereField("calendar_date", isGreaterThanOrEqualTo: startTimestamp)
+			.whereField("calendar_date", isLessThanOrEqualTo: endTimestamp)
+		
+		return try await withCheckedThrowingContinuation { continuation in
+			query.getDocuments { (querySnapshot, error) in
+				if let error = error {
+					continuation.resume(throwing: error)
+				} else {
+					do {
+						var records: [RecordResponseDTO] = []
+						for document in querySnapshot!.documents {
+							let data = document.data()
+							let jsonData = try JSONSerialization.data(withJSONObject: data,
+																												options: [])
+							let recordResponse = try JSONDecoder().decode(RecordResponseDTO.self,
+																														from: jsonData)
+							records.append(recordResponse)
+						}
+						continuation.resume(returning: records)
+					} catch {
+						continuation.resume(throwing: error)
 					}
+				}
 			}
+		}
 	}
 }
