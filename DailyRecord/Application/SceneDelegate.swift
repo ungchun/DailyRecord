e93d703d9964b41424fcdff0c285f99d8ac82822
sunghun <leedool3003@gmail.com>
2024-08-26 20:52:36 +0900

    :sparkles: [feat] 강제 업데이트 로직 추가 #26

--- DailyRecord/Application/SceneDelegate.swift
+++ DailyRecord/Application/SceneDelegate.swift
@@ -17,6 +17,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 	func scene(_ scene: UIScene,
 						 willConnectTo session: UISceneSession,
 						 options connectionOptions: UIScene.ConnectionOptions) {
+		checkForUpdate()
+		
 		if let windowScene = scene as? UIWindowScene {
 			if Auth.auth().currentUser == nil {
 				Auth.auth().signInAnonymously { authResult, error in
@@ -94,7 +96,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 	}
 	
 	func sceneWillEnterForeground(_ scene: UIScene) {
-		
+		checkForUpdate()
 	}
 	
 	func sceneDidEnterBackground(_ scene: UIScene) {
@@ -102,14 +104,76 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 	}
 }
 
-extension SceneDelegate {
-	private func initDisplayMode(_ window: UIWindow) {
+private extension SceneDelegate {
+	func initDisplayMode(_ window: UIWindow) {
 		if UserDefaultsSetting.currentDisplayMode == .system {
 			window.overrideUserInterfaceStyle = .unspecified
 		} else if UserDefaultsSetting.currentDisplayMode == .light {
 			window.overrideUserInterfaceStyle = .light
 		} else if UserDefaultsSetting.currentDisplayMode == .dark {
 			window.overrideUserInterfaceStyle = .dark
+		}
+	}
+	
+	func checkForUpdate() {
+		guard let currentVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"]
+						as? String else { return }
+		
+		guard let bundleId = Bundle.main.bundleIdentifier,
+					let url = URL(string: "https://itunes.apple.com/lookup?bundleId=\(bundleId)") else {
+			return
+		}
+		
+		let task = URLSession.shared.dataTask(with: url) { data, response, error in
+			guard let data = data, error == nil else {
+				print("Failed to fetch app info from App Store")
+				return
+			}
+			
+			if let json = try? JSONSerialization.jsonObject(with: data,
+																											options: []) as? [String: Any],
+				 let results = json["results"] as? [[String: Any]],
+				 let appStoreVersion = results.first?["version"] as? String {
+				DispatchQueue.main.async {
+					self.compareVersions(currentVersion: currentVersion,
+															 appStoreVersion: appStoreVersion)
+				}
+			}
+		}
+		
+		task.resume()
+	}
+	
+	func compareVersions(currentVersion: String, appStoreVersion: String) {
+		let currentVersionComponents = currentVersion.split(separator: ".").map { Int($0) ?? 0 }
+		let appStoreVersionComponents = appStoreVersion.split(separator: ".").map { Int($0) ?? 0 }
+		
+		if appStoreVersionComponents.count >= 2 {
+			if appStoreVersionComponents[0] > currentVersionComponents[0] ||
+					(appStoreVersionComponents[0] == currentVersionComponents[0]
+					 && appStoreVersionComponents[1] > currentVersionComponents[1]) {
+				self.showUpdateAlert()
+			}
+		}
+	}
+	
+	func showUpdateAlert() {
+		let alert = UIAlertController(
+			title: "업데이트 알림",
+			message: "더 나은 서비스를 위해 다온을 업데이트 해주세요!",
+			preferredStyle: .alert
+		)
+		alert.addAction(UIAlertAction(title: "업데이트", style: .default, handler: { _ in
+			if let url = URL(string: "https://apps.apple.com/app/6664067346"),
+				 UIApplication.shared.canOpenURL(url) {
+				UIApplication.shared.open(url)
+			}
+		}))
+		
+		if let windowScene = window?.windowScene {
+			windowScene.windows.first?.rootViewController?.present(alert,
+																														 animated: true,
+																														 completion: nil)
 		}
 	}
 }
