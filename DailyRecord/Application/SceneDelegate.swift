8547cf3ada8857ae0f7f1f6e7c9e4df1a1f2d8c9
sunghun <leedool3003@gmail.com>
2024-09-25 20:03:08 +0900

    :hammer: [chore] 기존 userdefault 익명 로직 변경

--- DailyRecord/Application/SceneDelegate.swift
+++ DailyRecord/Application/SceneDelegate.swift
@@ -11,206 +11,204 @@ import WidgetKit
 import FirebaseAuth
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-	
-	var window: UIWindow?
-	
-	func scene(_ scene: UIScene,
-						 willConnectTo session: UISceneSession,
-						 options connectionOptions: UIScene.ConnectionOptions) {
-		checkForUpdate()
-		
-		WidgetCenter.shared.reloadAllTimelines()
-		
-		if let windowScene = scene as? UIWindowScene {
-			if Auth.auth().currentUser == nil {
-				self.signInAnonymously(windowScene)
-			} else {
-				checkUserAgainstDatabase { [weak self] success, error in
-					if success {
-						self?.signInUser(windowScene)
-					} else {
-						/// 이미 탈퇴한 유저
-						self?.signInAnonymously(windowScene)
-					}
-				}
-			}
-		}
-	}
-	
-	func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
-		
-	}
-	
-	func sceneDidDisconnect(_ scene: UIScene) {
-		
-	}
-	
-	func sceneDidBecomeActive(_ scene: UIScene) {
-		
-	}
-	
-	func sceneWillResignActive(_ scene: UIScene) {
-		
-	}
-	
-	func sceneWillEnterForeground(_ scene: UIScene) {
-		checkForUpdate()
-	}
-	
-	func sceneDidEnterBackground(_ scene: UIScene) {
-		
-	}
+  var window: UIWindow?
+  
+  func scene(_ scene: UIScene,
+             willConnectTo session: UISceneSession,
+             options connectionOptions: UIScene.ConnectionOptions) {
+    checkForUpdate()
+    
+    WidgetCenter.shared.reloadAllTimelines()
+    
+    if let windowScene = scene as? UIWindowScene {
+      if Auth.auth().currentUser == nil {
+        self.signInAnonymously(windowScene)
+      } else {
+        checkUserAgainstDatabase { [weak self] success, error in
+          if success {
+            self?.signInUser(windowScene)
+          } else {
+            /// 이미 탈퇴한 유저
+            self?.signInAnonymously(windowScene)
+          }
+        }
+      }
+    }
+  }
+  
+  func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
+    
+  }
+  
+  func sceneDidDisconnect(_ scene: UIScene) {
+    
+  }
+  
+  func sceneDidBecomeActive(_ scene: UIScene) {
+    
+  }
+  
+  func sceneWillResignActive(_ scene: UIScene) {
+    
+  }
+  
+  func sceneWillEnterForeground(_ scene: UIScene) {
+    checkForUpdate()
+  }
+  
+  func sceneDidEnterBackground(_ scene: UIScene) {
+    
+  }
 }
 
 private extension SceneDelegate {
-	func signInAnonymously(_ windowScene: UIWindowScene) {
-		Auth.auth().signInAnonymously { authResult, error in
-			if let error = error {
-				Log.error(error)
-				return
-			}
-			
-			if let user = authResult?.user {
-				do {
-					try KeyChainManager.shared.create(account: .uid, data: user.uid)
-					UserDefaultsSetting.isAnonymously = true
-					UserDefaultsSetting.uid = user.uid
-					
-					var calendarDIContainer: CalendarDIContainer?
-					var calendarCoordinator: CalendarCoordinator?
-					let window = UIWindow(windowScene: windowScene)
-					self.window = window
-					
-					self.initDisplayMode(window)
-					
-					let navigationController = BaseNavigationController()
-					self.window?.rootViewController = navigationController
-					
-					calendarDIContainer = CalendarDIContainer(
-						navigationController: navigationController
-					)
-					calendarCoordinator = calendarDIContainer?.makeCalendarCoordinator()
-					calendarCoordinator?.start()
-					
-					self.window?.makeKeyAndVisible()
-				} catch {
-					Log.error(error)
-					exit(0)
-				}
-			}
-		}
-	}
-	
-	func signInUser(_ windowScene: UIWindowScene) {
-		if UserDefaultsSetting.uid.isEmpty {
-			do {
-				UserDefaultsSetting.uid = try KeyChainManager.shared.read(account: .uid)
-			} catch {
-				Log.error(error)
-			}
-		}
-		
-		var calendarDIContainer: CalendarDIContainer?
-		var calendarCoordinator: CalendarCoordinator?
-		let window = UIWindow(windowScene: windowScene)
-		self.window = window
-		
-		self.initDisplayMode(window)
-		
-		let navigationController = BaseNavigationController()
-		self.window?.rootViewController = navigationController
-		
-		calendarDIContainer = CalendarDIContainer(navigationController: navigationController)
-		calendarCoordinator = calendarDIContainer?.makeCalendarCoordinator()
-		calendarCoordinator?.start()
-		
-		self.window?.makeKeyAndVisible()
-	}
+  func signInAnonymously(_ windowScene: UIWindowScene) {
+    Auth.auth().signInAnonymously { authResult, error in
+      if let error = error {
+        Log.error(error)
+        return
+      }
+      
+      if let user = authResult?.user {
+        do {
+          try KeyChainManager.shared.create(account: .uid, data: user.uid)
+          UserDefaultsSetting.uid = user.uid
+          
+          var calendarDIContainer: CalendarDIContainer?
+          var calendarCoordinator: CalendarCoordinator?
+          let window = UIWindow(windowScene: windowScene)
+          self.window = window
+          
+          self.initDisplayMode(window)
+          
+          let navigationController = BaseNavigationController()
+          self.window?.rootViewController = navigationController
+          
+          calendarDIContainer = CalendarDIContainer(
+            navigationController: navigationController
+          )
+          calendarCoordinator = calendarDIContainer?.makeCalendarCoordinator()
+          calendarCoordinator?.start()
+          
+          self.window?.makeKeyAndVisible()
+        } catch {
+          Log.error(error)
+          exit(0)
+        }
+      }
+    }
+  }
+  
+  func signInUser(_ windowScene: UIWindowScene) {
+    if UserDefaultsSetting.uid.isEmpty {
+      do {
+        UserDefaultsSetting.uid = try KeyChainManager.shared.read(account: .uid)
+      } catch {
+        Log.error(error)
+      }
+    }
+    
+    var calendarDIContainer: CalendarDIContainer?
+    var calendarCoordinator: CalendarCoordinator?
+    let window = UIWindow(windowScene: windowScene)
+    self.window = window
+    
+    self.initDisplayMode(window)
+    
+    let navigationController = BaseNavigationController()
+    self.window?.rootViewController = navigationController
+    
+    calendarDIContainer = CalendarDIContainer(navigationController: navigationController)
+    calendarCoordinator = calendarDIContainer?.makeCalendarCoordinator()
+    calendarCoordinator?.start()
+    
+    self.window?.makeKeyAndVisible()
+  }
 }
 
 private extension SceneDelegate {
-	func checkUserAgainstDatabase(completion: @escaping (_ success: Bool,
-																											 _ error: NSError?) -> Void) {
-		guard let currentUser = Auth.auth().currentUser else { return }
-		currentUser.getIDTokenForcingRefresh(true, completion:  { (idToken, error) in
-			if let error = error {
-				completion(false, error as NSError?)
-				print(error.localizedDescription)
-			} else {
-				completion(true, nil)
-			}
-		})
-	}
-	
-	func initDisplayMode(_ window: UIWindow) {
-		if UserDefaultsSetting.currentDisplayMode == .system {
-			window.overrideUserInterfaceStyle = .unspecified
-		} else if UserDefaultsSetting.currentDisplayMode == .light {
-			window.overrideUserInterfaceStyle = .light
-		} else if UserDefaultsSetting.currentDisplayMode == .dark {
-			window.overrideUserInterfaceStyle = .dark
-		}
-	}
-	
-	func checkForUpdate() {
-		guard let currentVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"]
-						as? String else { return }
-		
-		guard let bundleId = Bundle.main.bundleIdentifier,
-					let url = URL(string: "https://itunes.apple.com/lookup?bundleId=\(bundleId)") else {
-			return
-		}
-		
-		let task = URLSession.shared.dataTask(with: url) { data, response, error in
-			guard let data = data, error == nil else {
-				print("Failed to fetch app info from App Store")
-				return
-			}
-			
-			if let json = try? JSONSerialization.jsonObject(with: data,
-																											options: []) as? [String: Any],
-				 let results = json["results"] as? [[String: Any]],
-				 let appStoreVersion = results.first?["version"] as? String {
-				DispatchQueue.main.async {
-					self.compareVersions(currentVersion: currentVersion,
-															 appStoreVersion: appStoreVersion)
-				}
-			}
-		}
-		
-		task.resume()
-	}
-	
-	func compareVersions(currentVersion: String, appStoreVersion: String) {
-		let currentVersionComponents = currentVersion.split(separator: ".").map { Int($0) ?? 0 }
-		let appStoreVersionComponents = appStoreVersion.split(separator: ".").map { Int($0) ?? 0 }
-		
-		if appStoreVersionComponents.count >= 2 {
-			if appStoreVersionComponents[0] > currentVersionComponents[0] ||
-					(appStoreVersionComponents[0] == currentVersionComponents[0]
-					 && appStoreVersionComponents[1] > currentVersionComponents[1]) {
-				self.showUpdateAlert()
-			}
-		}
-	}
-	
-	func showUpdateAlert() {
-		let alert = UIAlertController(
-			title: "업데이트 알림",
-			message: "더 나은 서비스를 위해 다온을 업데이트 해주세요!",
-			preferredStyle: .alert
-		)
-		alert.addAction(UIAlertAction(title: "업데이트", style: .default, handler: { _ in
-			if let url = URL(string: "https://apps.apple.com/app/6664067346"),
-				 UIApplication.shared.canOpenURL(url) {
-				UIApplication.shared.open(url)
-			}
-		}))
-		
-		if let windowScene = window?.windowScene {
-			windowScene.windows.first?.rootViewController?.present(alert,
-																														 animated: true,
-																														 completion: nil)
-		}
-	}
+  func checkUserAgainstDatabase(completion: @escaping (_ success: Bool,
+                                                       _ error: NSError?) -> Void) {
+    guard let currentUser = Auth.auth().currentUser else { return }
+    currentUser.getIDTokenForcingRefresh(true, completion:  { (idToken, error) in
+      if let error = error {
+        completion(false, error as NSError?)
+        print(error.localizedDescription)
+      } else {
+        completion(true, nil)
+      }
+    })
+  }
+  
+  func initDisplayMode(_ window: UIWindow) {
+    if UserDefaultsSetting.currentDisplayMode == .system {
+      window.overrideUserInterfaceStyle = .unspecified
+    } else if UserDefaultsSetting.currentDisplayMode == .light {
+      window.overrideUserInterfaceStyle = .light
+    } else if UserDefaultsSetting.currentDisplayMode == .dark {
+      window.overrideUserInterfaceStyle = .dark
+    }
+  }
+  
+  func checkForUpdate() {
+    guard let currentVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"]
+            as? String else { return }
+    
+    guard let bundleId = Bundle.main.bundleIdentifier,
+          let url = URL(string: "https://itunes.apple.com/lookup?bundleId=\(bundleId)") else {
+      return
+    }
+    
+    let task = URLSession.shared.dataTask(with: url) { data, response, error in
+      guard let data = data, error == nil else {
+        print("Failed to fetch app info from App Store")
+        return
+      }
+      
+      if let json = try? JSONSerialization.jsonObject(with: data,
+                                                      options: []) as? [String: Any],
+         let results = json["results"] as? [[String: Any]],
+         let appStoreVersion = results.first?["version"] as? String {
+        DispatchQueue.main.async {
+          self.compareVersions(currentVersion: currentVersion,
+                               appStoreVersion: appStoreVersion)
+        }
+      }
+    }
+    
+    task.resume()
+  }
+  
+  func compareVersions(currentVersion: String, appStoreVersion: String) {
+    let currentVersionComponents = currentVersion.split(separator: ".").map { Int($0) ?? 0 }
+    let appStoreVersionComponents = appStoreVersion.split(separator: ".").map { Int($0) ?? 0 }
+    
+    if appStoreVersionComponents.count >= 2 {
+      if appStoreVersionComponents[0] > currentVersionComponents[0] ||
+          (appStoreVersionComponents[0] == currentVersionComponents[0]
+           && appStoreVersionComponents[1] > currentVersionComponents[1]) {
+        self.showUpdateAlert()
+      }
+    }
+  }
+  
+  func showUpdateAlert() {
+    let alert = UIAlertController(
+      title: "업데이트 알림",
+      message: "더 나은 서비스를 위해 다온을 업데이트 해주세요!",
+      preferredStyle: .alert
+    )
+    alert.addAction(UIAlertAction(title: "업데이트", style: .default, handler: { _ in
+      if let url = URL(string: "https://apps.apple.com/app/6664067346"),
+         UIApplication.shared.canOpenURL(url) {
+        UIApplication.shared.open(url)
+      }
+    }))
+    
+    if let windowScene = window?.windowScene {
+      windowScene.windows.first?.rootViewController?.present(alert,
+                                                             animated: true,
+                                                             completion: nil)
+    }
+  }
 }
--- DailyRecord/Presentation/ProfileScene/ViewController/ProfileViewController.swift
+++ DailyRecord/Presentation/ProfileScene/ViewController/ProfileViewController.swift
@@ -13,236 +13,240 @@ import WidgetKit
 import FirebaseAuth
 
 final class ProfileViewController: BaseViewController {
-	
-	// MARK: - Properties
-	
-	var coordinator: ProfileCoordinator?
-	
-	private let viewModel: ProfileViewModel
-	private let calendarViewModel: CalendarViewModel
-	private let appleSignInService = AppleSignInService.shared
-	
-	private static let reuseIdentifier: String = "ProfileCell"
-	
-	// MARK: - Views
-	
-	private let tableView: UITableView = {
-		let tableView = UITableView()
-		tableView.translatesAutoresizingMaskIntoConstraints = false
-		tableView.register(
-			UITableViewCell.self,
-			forCellReuseIdentifier: ProfileViewController.reuseIdentifier
-		)
-		return tableView
-	}()
-	
-	// MARK: - Init
-	
-	init(
-		viewModel: ProfileViewModel,
-		calendarViewModel: CalendarViewModel
-	) {
-		self.viewModel = viewModel
-		self.calendarViewModel = calendarViewModel
-		super.init(nibName: nil, bundle: nil)
-	}
-	
-	required init?(coder: NSCoder) {
-		fatalError("init(coder:) has not been implemented")
-	}
-	
-	// MARK: - Life Cycle
-	
-	override func viewDidLoad() {
-		super.viewDidLoad()
-	}
-	
-	// MARK: - Functions
-	
-	override func addView() {
-		[tableView].forEach {
-			view.addSubview($0)
-		}
-	}
-	
-	override func setLayout() {
-		tableView.snp.makeConstraints { make in
-			make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
-			make.leading.equalToSuperview()
-			make.trailing.equalToSuperview()
-			make.bottom.equalToSuperview()
-		}
-	}
-	
-	override func setupView() {
-		view.backgroundColor = .azBlack
-		
-		tableView.dataSource = self
-		tableView.delegate = self
-		tableView.backgroundColor = .azBlack
-		tableView.separatorStyle = .none
-		tableView.isScrollEnabled = false
-	}
+  
+  // MARK: - Properties
+  
+  var coordinator: ProfileCoordinator?
+  
+  private let viewModel: ProfileViewModel
+  private let calendarViewModel: CalendarViewModel
+  private let appleSignInService = AppleSignInService.shared
+  
+  private var isAnonymously: Bool = false
+  
+  private static let reuseIdentifier: String = "ProfileCell"
+  
+  // MARK: - Views
+  
+  private let tableView: UITableView = {
+    let tableView = UITableView()
+    tableView.translatesAutoresizingMaskIntoConstraints = false
+    tableView.register(
+      UITableViewCell.self,
+      forCellReuseIdentifier: ProfileViewController.reuseIdentifier
+    )
+    return tableView
+  }()
+  
+  // MARK: - Init
+  
+  init(
+    viewModel: ProfileViewModel,
+    calendarViewModel: CalendarViewModel
+  ) {
+    self.viewModel = viewModel
+    self.calendarViewModel = calendarViewModel
+    super.init(nibName: nil, bundle: nil)
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  // MARK: - Life Cycle
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+  // MARK: - Functions
+  
+  override func addView() {
+    [tableView].forEach {
+      view.addSubview($0)
+    }
+  }
+  
+  override func setLayout() {
+    tableView.snp.makeConstraints { make in
+      make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+      make.leading.equalToSuperview()
+      make.trailing.equalToSuperview()
+      make.bottom.equalToSuperview()
+    }
+  }
+  
+  override func setupView() {
+    view.backgroundColor = .azBlack
+    
+    tableView.dataSource = self
+    tableView.delegate = self
+    tableView.backgroundColor = .azBlack
+    tableView.separatorStyle = .none
+    tableView.isScrollEnabled = false
+    
+    isAnonymously = Auth.auth().currentUser?.isAnonymous ?? false
+  }
 }
 
 extension ProfileViewController: UITableViewDataSource, UITableViewDelegate {
-	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-		return viewModel.profileCellItems.count - 1
-	}
-	
-	func tableView(_ tableView: UITableView,
-								 cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-		let cell = tableView.dequeueReusableCell(
-			withIdentifier: ProfileViewController.reuseIdentifier,
-			for: indexPath
-		)
-		
-		let items: [ProfileCellItem] = UserDefaultsSetting.isAnonymously
-		? [.appleLogin, .darkMode, .deleteAccount]
-		: [.appleLoginComplete, .darkMode, .deleteAccount]
-		
-		let item = items[indexPath.row]
-		
-		cell.textLabel?.text = item.rawValue
-		cell.textLabel?.font = UIFont(name: "omyu_pretty", size: 16)
-		cell.textLabel?.textColor = item == .deleteAccount ? .red : .azWhite
-		
-		let config = UIImage.SymbolConfiguration(pointSize: 16, weight: .bold, scale: .default)
-		cell.imageView?.image = UIImage(systemName: item.iconName, withConfiguration: config)
-		cell.imageView?.tintColor = item == .deleteAccount ? .red : .azWhite
-		
-		cell.selectionStyle = .none
-		cell.backgroundColor = .azBlack
-		return cell
-	}
-	
-	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-		tableView.deselectRow(at: indexPath, animated: true)
-		
-		let items: [ProfileCellItem] = UserDefaultsSetting.isAnonymously
-		? [.appleLogin, .darkMode, .deleteAccount]
-		: [.appleLoginComplete, .darkMode, .deleteAccount]
-		
-		let selectedItem = items[indexPath.row]
-		
-		switch selectedItem {
-		case .appleLogin:
-			appleLoginTrigger()
-		case .darkMode:
-			darkModeTrigger()
-		case .deleteAccount:
-			showRemoveUserAlert()
-		default:
-			break
-		}
-	}
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return viewModel.profileCellItems.count - 2
+  }
+  
+  func tableView(_ tableView: UITableView,
+                 cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(
+      withIdentifier: ProfileViewController.reuseIdentifier,
+      for: indexPath
+    )
+    
+    let items: [ProfileCellItem] = isAnonymously
+    ? [.appleLogin, .darkMode]
+    : [.darkMode, .deleteAccount]
+    
+    let item = items[indexPath.row]
+    
+    cell.textLabel?.text = item.rawValue
+    cell.textLabel?.font = UIFont(name: "omyu_pretty", size: 16)
+    cell.textLabel?.textColor = item == .deleteAccount ? .red : .azWhite
+    
+    let config = UIImage.SymbolConfiguration(pointSize: 16, weight: .bold, scale: .default)
+    cell.imageView?.image = UIImage(systemName: item.iconName, withConfiguration: config)
+    cell.imageView?.tintColor = item == .deleteAccount ? .red : .azWhite
+    
+    cell.selectionStyle = .none
+    cell.backgroundColor = .azBlack
+    return cell
+  }
+  
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    tableView.deselectRow(at: indexPath, animated: true)
+    
+    let items: [ProfileCellItem] = isAnonymously
+    ? [.appleLogin, .darkMode]
+    : [.darkMode, .deleteAccount]
+    
+    let selectedItem = items[indexPath.row]
+    
+    switch selectedItem {
+    case .appleLogin:
+      appleLoginTrigger()
+    case .darkMode:
+      darkModeTrigger()
+    case .deleteAccount:
+      showRemoveUserAlert()
+    default:
+      break
+    }
+  }
 }
 
 extension ProfileViewController {
-	private func appleLoginTrigger() {
-		Task {
-			let credential = try await appleSignInService.startSignInWithAppleFlow()
-			if Auth.auth().currentUser?.isAnonymous == true {
-				try await linkAnonymousAccountWithApple(credential: credential)
-			} else {
-				try await signInWithCredential(credential)
-			}
-		}
-	}
-	
-	private func linkAnonymousAccountWithApple(credential: AuthCredential) async throws {
-		do {
-			let authResult = try await Auth.auth().currentUser?.link(with: credential)
-			try await handleAuthResult(authResult)
-		} catch {
-			/// https://github.com/firebase/firebase-ios-sdk/issues/12146
-			let authError = error as? AuthErrorCode
-			if let newCredential = authError?.userInfo[AuthErrorUserInfoUpdatedCredentialKey]
-					as? AuthCredential {
-				let authResult = try await Auth.auth().signIn(with: newCredential)
-				try await handleAuthResult(authResult)
-			} else {
-				handleError(self.coordinator!, "사용자 데이터 생성 중 오류가 발생했어요")
-			}
-		}
-	}
-	
-	private func signInWithCredential(_ credential: AuthCredential) async throws {
-		do {
-			let authResult = try await Auth.auth().signIn(with: credential)
-			try await handleAuthResult(authResult)
-		} catch {
-			handleError(self.coordinator!, "사용자 데이터 생성 중 오류가 발생했어요")
-		}
-	}
-	
-	private func handleAuthResult(_ authResult: AuthDataResult?) async throws {
-		appleSignInService.handleAuthorizationResult(
-			authResult: authResult, error: nil
-		) { [weak self] _ in
-			guard let self else { return }
-			Task {
-				do {
-					try await self.createUserAndFetchRecord()
-				} catch {
-					self.handleError(self.coordinator!, "사용자 데이터 생성 중 오류가 발생했어요")
-				}
-			}
-		}
-	}
-	
-	private func createUserAndFetchRecord() async throws {
-		do {
-			try await viewModel.createUserTirgger()
-			if let year = Int(formattedDateString(Date(), format: "yyyy")),
-				 let month = Int(formattedDateString(Date(), format: "M")) {
-				try await calendarViewModel.fetchMonthRecordTrigger(year: year, month: month) {
-					WidgetCenter.shared.reloadAllTimelines()
-					
-					self.handleError(self.coordinator!, "애플 로그인 연동 성공")
-					
-					self.coordinator?.popToRoot()
-				}
-			}
-		} catch {
-			handleError(self.coordinator!, "사용자 데이터 생성 중 오류가 발생했어요")
-		}
-	}
-	
-	private func darkModeTrigger() {
-		coordinator?.showSetDarkmode()
-	}
-	
-	private func showRemoveUserAlert() {
-		let alertController = UIAlertController(
-			title: "회원 탈퇴",
-			message: "정말 탈퇴 하시겠어요? 회원 탈퇴 후 복구는 어려워요",
-			preferredStyle: .alert
-		)
-		
-		let cancelAction = UIAlertAction(title: "탈퇴하기", style: .destructive) { _ in
-			Task { [weak self] in
-				guard let self else { return }
-				do {
-					try await self.viewModel.removeUserTrigger()
-				} catch {
-					handleError(self.coordinator!, "에러가 발생했어요")
-				}
-			}
-		}
-		alertController.addAction(cancelAction)
-		
-		let deleteAction = UIAlertAction(title: "취소", style: .default) { _ in }
-		alertController.addAction(deleteAction)
-		
-		present(alertController, animated: true, completion: nil)
-	}
-	
-	private func formattedDateString(_ date: Date, format: String) -> String {
-		let dateFormatter = DateFormatter()
-		dateFormatter.locale = Locale(identifier: "ko_kr")
-		dateFormatter.timeZone = TimeZone(identifier: "KST")
-		dateFormatter.dateFormat = format
-		return dateFormatter.string(from: date)
-	}
+  private func appleLoginTrigger() {
+    Task {
+      let credential = try await appleSignInService.startSignInWithAppleFlow()
+      if Auth.auth().currentUser?.isAnonymous == true {
+        try await linkAnonymousAccountWithApple(credential: credential)
+      } else {
+        try await signInWithCredential(credential)
+      }
+    }
+  }
+  
+  private func linkAnonymousAccountWithApple(credential: AuthCredential) async throws {
+    do {
+      let authResult = try await Auth.auth().currentUser?.link(with: credential)
+      try await handleAuthResult(authResult)
+    } catch {
+      /// https://github.com/firebase/firebase-ios-sdk/issues/12146
+      let authError = error as? AuthErrorCode
+      if let newCredential = authError?.userInfo[AuthErrorUserInfoUpdatedCredentialKey]
+          as? AuthCredential {
+        let authResult = try await Auth.auth().signIn(with: newCredential)
+        try await handleAuthResult(authResult)
+      } else {
+        handleError(self.coordinator!, "사용자 데이터 생성 중 오류가 발생했어요")
+      }
+    }
+  }
+  
+  private func signInWithCredential(_ credential: AuthCredential) async throws {
+    do {
+      let authResult = try await Auth.auth().signIn(with: credential)
+      try await handleAuthResult(authResult)
+    } catch {
+      handleError(self.coordinator!, "사용자 데이터 생성 중 오류가 발생했어요")
+    }
+  }
+  
+  private func handleAuthResult(_ authResult: AuthDataResult?) async throws {
+    appleSignInService.handleAuthorizationResult(
+      authResult: authResult, error: nil
+    ) { [weak self] _ in
+      guard let self else { return }
+      Task {
+        do {
+          try await self.createUserAndFetchRecord()
+        } catch {
+          self.handleError(self.coordinator!, "사용자 데이터 생성 중 오류가 발생했어요")
+        }
+      }
+    }
+  }
+  
+  private func createUserAndFetchRecord() async throws {
+    do {
+      try await viewModel.createUserTirgger()
+      if let year = Int(formattedDateString(Date(), format: "yyyy")),
+         let month = Int(formattedDateString(Date(), format: "M")) {
+        try await calendarViewModel.fetchMonthRecordTrigger(year: year, month: month) {
+          WidgetCenter.shared.reloadAllTimelines()
+          
+          self.handleError(self.coordinator!, "애플 로그인 연동 성공")
+          
+          self.coordinator?.popToRoot()
+        }
+      }
+    } catch {
+      handleError(self.coordinator!, "사용자 데이터 생성 중 오류가 발생했어요")
+    }
+  }
+  
+  private func darkModeTrigger() {
+    coordinator?.showSetDarkmode()
+  }
+  
+  private func showRemoveUserAlert() {
+    let alertController = UIAlertController(
+      title: "회원 탈퇴",
+      message: "정말 탈퇴 하시겠어요? 회원 탈퇴 후 복구는 어려워요",
+      preferredStyle: .alert
+    )
+    
+    let cancelAction = UIAlertAction(title: "탈퇴하기", style: .destructive) { _ in
+      Task { [weak self] in
+        guard let self else { return }
+        do {
+          try await self.viewModel.removeUserTrigger()
+        } catch {
+          handleError(self.coordinator!, "에러가 발생했어요")
+        }
+      }
+    }
+    alertController.addAction(cancelAction)
+    
+    let deleteAction = UIAlertAction(title: "취소", style: .default) { _ in }
+    alertController.addAction(deleteAction)
+    
+    present(alertController, animated: true, completion: nil)
+  }
+  
+  private func formattedDateString(_ date: Date, format: String) -> String {
+    let dateFormatter = DateFormatter()
+    dateFormatter.locale = Locale(identifier: "ko_kr")
+    dateFormatter.timeZone = TimeZone(identifier: "KST")
+    dateFormatter.dateFormat = format
+    return dateFormatter.string(from: date)
+  }
 }
