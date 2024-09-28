a6252fd462327039ac345a86cfefb3281ed176de
sunghun <leedool3003@gmail.com>
2024-09-28 14:16:21 +0900

    :sparkles: [feat] profile firebase -> coredata로 로직 수정 #55

--- DailyRecord/Presentation/ProfileScene/ViewController/ProfileViewController.swift
+++ DailyRecord/Presentation/ProfileScene/ViewController/ProfileViewController.swift
@@ -10,8 +10,6 @@ import CryptoKit
 import UIKit
 import WidgetKit
 
-import FirebaseAuth
-
 final class ProfileViewController: BaseViewController {
   
   // MARK: - Properties
@@ -20,9 +18,6 @@ final class ProfileViewController: BaseViewController {
   
   private let viewModel: ProfileViewModel
   private let calendarViewModel: CalendarViewModel
-  private let appleSignInService = AppleSignInService.shared
-  
-  private var isAnonymously: Bool = false
   
   private static let reuseIdentifier: String = "ProfileCell"
   
@@ -84,15 +79,12 @@ final class ProfileViewController: BaseViewController {
     tableView.backgroundColor = .azBlack
     tableView.separatorStyle = .none
     tableView.isScrollEnabled = false
-    
-    isAnonymously = Auth.auth().currentUser?.isAnonymous ?? false
   }
 }
 
 extension ProfileViewController: UITableViewDataSource, UITableViewDelegate {
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return isAnonymously
-    ? viewModel.profileCellItems.count - 2 : viewModel.profileCellItems.count - 1
+    return viewModel.profileCellItems.count
   }
   
   func tableView(_ tableView: UITableView,
@@ -102,23 +94,17 @@ extension ProfileViewController: UITableViewDataSource, UITableViewDelegate {
       for: indexPath
     )
     
-    let items: [ProfileCellItem] = isAnonymously
-    ? [.appleLogin, .darkMode]
-    : [.appleLoginComplete, .darkMode, .deleteAccount]
+    let items: [ProfileCellItem] = [.iCloud, .darkMode]
     
     let item = items[indexPath.row]
     
     cell.textLabel?.text = item.rawValue
     cell.textLabel?.font = UIFont(name: "omyu_pretty", size: 16)
-    cell.textLabel?.textColor = item == .deleteAccount ? .red : .azWhite
+    cell.textLabel?.textColor = .azWhite
     
-    if item == .appleLoginComplete || item == .appleLogin {
-      cell.imageView?.image = UIImage(named: item.iconName)
-    } else {
-      let config = UIImage.SymbolConfiguration(pointSize: 16, weight: .bold, scale: .default)
-      cell.imageView?.image = UIImage(systemName: item.iconName, withConfiguration: config)
-    }
-    cell.imageView?.tintColor = item == .deleteAccount ? .red : .azWhite
+    let config = UIImage.SymbolConfiguration(pointSize: 16, weight: .bold, scale: .default)
+    cell.imageView?.image = UIImage(systemName: item.iconName, withConfiguration: config)
+    cell.imageView?.tintColor = .azWhite
     
     cell.selectionStyle = .none
     cell.backgroundColor = .azBlack
@@ -128,135 +114,34 @@ extension ProfileViewController: UITableViewDataSource, UITableViewDelegate {
   func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
     tableView.deselectRow(at: indexPath, animated: true)
     
-    let items: [ProfileCellItem] = isAnonymously
-    ? [.appleLogin, .darkMode]
-    : [.appleLoginComplete, .darkMode, .deleteAccount]
+    let items: [ProfileCellItem] = [.iCloud, .darkMode]
     
     let selectedItem = items[indexPath.row]
     
     switch selectedItem {
-    case .appleLogin:
-      appleLoginTrigger()
+    case .iCloud:
+      showiCloudAlert()
     case .darkMode:
       darkModeTrigger()
-    case .deleteAccount:
-      showRemoveUserAlert()
-    default:
-      break
     }
   }
 }
 
 extension ProfileViewController {
-  private func appleLoginTrigger() {
-    Task {
-      do {
-        let credential = try await appleSignInService.startSignInWithAppleFlow()
-        if Auth.auth().currentUser?.isAnonymous == true {
-          try await linkAnonymousAccountWithApple(credential: credential)
-        } else {
-          try await signInWithCredential(credential)
-        }
-      } catch {
-        handleError(self.coordinator!, "애플 로그인 에러 발생")
-      }
-    }
-  }
-  
-  private func linkAnonymousAccountWithApple(credential: AuthCredential) async throws {
-    do {
-      let authResult = try await Auth.auth().currentUser?.link(with: credential)
-      try await handleAuthResult(authResult)
-    } catch {
-      /// https://github.com/firebase/firebase-ios-sdk/issues/12146
-      let authError = error as? AuthErrorCode
-      if let newCredential = authError?.userInfo[AuthErrorUserInfoUpdatedCredentialKey]
-          as? AuthCredential {
-        let authResult = try await Auth.auth().signIn(with: newCredential)
-        try await handleAuthResult(authResult)
-      } else {
-        handleError(self.coordinator!, "사용자 데이터 생성 중 오류가 발생했어요")
-      }
-    }
-  }
-  
-  private func signInWithCredential(_ credential: AuthCredential) async throws {
-    do {
-      let authResult = try await Auth.auth().signIn(with: credential)
-      try await handleAuthResult(authResult)
-    } catch {
-      handleError(self.coordinator!, "사용자 데이터 생성 중 오류가 발생했어요")
-    }
-  }
-  
-  private func handleAuthResult(_ authResult: AuthDataResult?) async throws {
-    appleSignInService.handleAuthorizationResult(
-      authResult: authResult, error: nil
-    ) { [weak self] _ in
-      guard let self else { return }
-      Task {
-        do {
-          try await self.createUserAndFetchRecord()
-        } catch {
-          self.handleError(self.coordinator!, "사용자 데이터 생성 중 오류가 발생했어요")
-        }
-      }
-    }
-  }
-  
-  private func createUserAndFetchRecord() async throws {
-    do {
-      try await viewModel.createUserTirgger()
-      if let year = Int(formattedDateString(Date(), format: "yyyy")),
-         let month = Int(formattedDateString(Date(), format: "M")) {
-        try await calendarViewModel.fetchMonthRecordTrigger(year: year, month: month) {
-          WidgetCenter.shared.reloadAllTimelines()
-          
-          self.handleError(self.coordinator!, "애플 로그인 연동 성공")
-          
-          self.coordinator?.popToRoot()
-        }
-      }
-    } catch {
-      handleError(self.coordinator!, "사용자 데이터 생성 중 오류가 발생했어요")
-    }
-  }
-  
-  private func darkModeTrigger() {
-    coordinator?.showSetDarkmode()
-  }
-  
-  private func showRemoveUserAlert() {
+  private func showiCloudAlert() {
     let alertController = UIAlertController(
-      title: "회원 탈퇴",
-      message: "정말 탈퇴 하시겠어요? 회원 탈퇴 후 복구는 어려워요",
+      title: "iCloud 동기화",
+      message: "iCloud 동기화 기능이 개발중에 있어요",
       preferredStyle: .alert
     )
     
-    let cancelAction = UIAlertAction(title: "탈퇴하기", style: .destructive) { _ in
-      Task { [weak self] in
-        guard let self else { return }
-        do {
-          try await self.viewModel.removeUserTrigger()
-        } catch {
-          // 에러 발생
-        }
-      }
-    }
-    
-    alertController.addAction(cancelAction)
-    
-    let deleteAction = UIAlertAction(title: "취소", style: .default) { _ in }
+    let deleteAction = UIAlertAction(title: "확인", style: .default) { _ in }
     alertController.addAction(deleteAction)
     
     present(alertController, animated: true, completion: nil)
   }
   
-  private func formattedDateString(_ date: Date, format: String) -> String {
-    let dateFormatter = DateFormatter()
-    dateFormatter.locale = Locale(identifier: "ko_kr")
-    dateFormatter.timeZone = TimeZone(identifier: "KST")
-    dateFormatter.dateFormat = format
-    return dateFormatter.string(from: date)
+  private func darkModeTrigger() {
+    coordinator?.showSetDarkmode()
   }
 }
--- DailyRecord/Presentation/ProfileScene/ViewModel/ProfileViewModel.swift
+++ DailyRecord/Presentation/ProfileScene/ViewModel/ProfileViewModel.swift
@@ -7,77 +7,33 @@
 
 import Foundation
 
-import FirebaseAuth
-
-enum LoginPlatForm: String {
-	case apple
-}
-
 enum ProfileCellItem: String, CaseIterable {
-	case appleLoginComplete = "애플 로그인 연동 완료"
-	case appleLogin = "애플 로그인 연동"
-	case darkMode = "다크 모드"
-	case deleteAccount = "회원 탈퇴"
+  case iCloud = "iCloud 동기화"
+  case darkMode = "다크 모드"
   
-	var iconName: String {
-		switch self {
-		case .appleLoginComplete:
-      return "appleid_button"
-		case .appleLogin:
-			return "appleid_button"
-		case .darkMode:
-			return "moon"
-		case .deleteAccount:
-			return "trash"
-		}
-	}
+  var iconName: String {
+    switch self {
+    case .iCloud:
+      return "icloud"
+    case .darkMode:
+      return "moon"
+    }
+  }
 }
 
 final class ProfileViewModel: BaseViewModel {
-	
-	// MARK: - Properties
-	
-	let profileCellItems = ProfileCellItem.allCases.map { $0.rawValue }
-	
-	private let profileUseCase: DefaultProfileUseCase
-	
-	private let randomNickname: [String] = [
-		"햇살러버", "포근한위로", "따스한하루", "솔라스마일", "온기드림",
-		"블루스카이", "선샤인브리즈", "윈터블룸", "미스틱모먼트", "글로우위시"
-	]
-	
-	// MARK: - Init
-	
-	init(
-		profileUseCase: DefaultProfileUseCase
-	) {
-		self.profileUseCase = profileUseCase
-	}
-}
-
-extension ProfileViewModel {
-	
-	// MARK: - Functions
-	
-	func createUserTirgger() async throws {
-		guard let userID = Auth.auth().currentUser?.uid else { return }
-		let resposne = try await profileUseCase.getUserInfo()
-		
-		if resposne == nil {
-			let userRequest = UserRequest(uid: "",
-																		nickname: randomNickname.randomElement()!,
-																		platform: LoginPlatForm.apple.rawValue,
-																		fcm_token: "")
-			let userData = try userRequest.asDictionary()
-			
-			try await profileUseCase.createUser(data: userData)
-		} else {
-			try KeyChainManager.shared.create(account: .uid, data: userID)
-			UserDefaultsSetting.uid = userID
-		}
-	}
-	
-	func removeUserTrigger() async throws {
-		try await profileUseCase.removeUser()
-	}
+  
+  // MARK: - Properties
+  
+  let profileCellItems = ProfileCellItem.allCases.map { $0.rawValue }
+  
+  private let profileUseCase: DefaultProfileUseCase
+  
+  // MARK: - Init
+  
+  init(
+    profileUseCase: DefaultProfileUseCase
+  ) {
+    self.profileUseCase = profileUseCase
+  }
 }
