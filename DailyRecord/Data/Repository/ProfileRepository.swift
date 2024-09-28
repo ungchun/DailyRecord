0c23e4e3f3e5453e21d9b21c4ac457f14221636d
sunghun <leedool3003@gmail.com>
2024-09-29 00:16:50 +0900

    :recycle: [refactor] 안 쓰는 코드 삭제

--- DailyRecord/Data/Repository/ProfileRepository.swift
+++ DailyRecord/Data/Repository/ProfileRepository.swift
@@ -5,10 +5,7 @@
 //  Created by Kim SungHun on 7/7/24.
 //
 
-import Firebase
-import FirebaseFirestore
-
-final class ProfileRepository: NSObject, DefaultProfileRepository {
+final class ProfileRepository: DefaultProfileRepository {
   
 }
 
