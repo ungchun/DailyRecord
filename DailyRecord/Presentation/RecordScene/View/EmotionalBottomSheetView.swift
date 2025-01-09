fc81531f5f008f439c668c18a97cc9350a8e828f
sunghun <leedool3003@gmail.com>
2025-01-09 19:45:50 +0900

    :sparkles: [feat] 감정표현 바텀시트로 변경 #70

--- DailyRecord/Presentation/RecordScene/View/EmotionalBottomSheetView.swift
+++ DailyRecord/Presentation/RecordScene/View/EmotionalBottomSheetView.swift
@@ -1,5 +1,5 @@
 //
-//  EmotionalImagePopupView.swift
+//  EmotionalBottomSheetView.swift
 //  DailyRecord
 //
 //  Created by Kim SungHun on 6/20/24.
@@ -9,251 +9,352 @@ import UIKit
 
 import SnapKit
 
+protocol EmotionalBottomSheetViewViewDelegate: AnyObject {
+  func emotionalImageTapTrigger(selectEmotionType: EmotionType)
+}
 
+final class EmotionalBottomSheetViewController: BaseViewController {
+  
+  // MARK: - Properties
+  
+  weak var delegate: EmotionalBottomSheetViewViewDelegate?
+  
+  // MARK: - Views
+  
+  private let containerView: UIView = {
+    let view = UIView()
+    view.backgroundColor = .azBlack
+    return view
+  }()
+  
+  private let veryHappyEmotion: UIImageView = {
+    let view = UIImageView()
+    view.image = UIImage(named: "very_happy")
+    view.sizeToFit()
+    view.isUserInteractionEnabled = true
+    return view
+  }()
+  
+  private let happyEmotion: UIImageView = {
+    let view = UIImageView()
+    view.image = UIImage(named: "happy")
+    view.sizeToFit()
+    view.isUserInteractionEnabled = true
+    return view
+  }()
+  
+  private let neutralEmotion: UIImageView = {
+    let view = UIImageView()
+    view.image = UIImage(named: "neutral")
+    view.sizeToFit()
+    view.isUserInteractionEnabled = true
+    return view
+  }()
+  
+  private let verySadEmotion: UIImageView = {
+    let view = UIImageView()
+    view.image = UIImage(named: "very_sad")
+    view.sizeToFit()
+    view.isUserInteractionEnabled = true
+    return view
+  }()
+  
+  private let sadEmotion: UIImageView = {
+    let view = UIImageView()
+    view.image = UIImage(named: "sad")
+    view.sizeToFit()
+    view.isUserInteractionEnabled = true
+    return view
+  }()
+  
+  private let angryEmotion: UIImageView = {
+    let view = UIImageView()
+    view.image = UIImage(named: "angry")
+    view.sizeToFit()
+    view.isUserInteractionEnabled = true
+    return view
+  }()
+  
+  private let embarrassedEmotion: UIImageView = {
+    let view = UIImageView()
+    view.image = UIImage(named: "embarrassed")
+    view.sizeToFit()
+    view.isUserInteractionEnabled = true
+    return view
+  }()
+  
+  private let hurtEmotion: UIImageView = {
+    let view = UIImageView()
+    view.image = UIImage(named: "hurt")
+    view.sizeToFit()
+    view.isUserInteractionEnabled = true
+    return view
+  }()
+  
+  private let lovelyEmotion: UIImageView = {
+    let view = UIImageView()
+    view.image = UIImage(named: "lovely")
+    view.sizeToFit()
+    view.isUserInteractionEnabled = true
+    return view
+  }()
+  
+  private let sleepyEmotion: UIImageView = {
+    let view = UIImageView()
+    view.image = UIImage(named: "sleepy")
+    view.sizeToFit()
+    view.isUserInteractionEnabled = true
+    return view
+  }()
+  
+  private let surprisedEmotion: UIImageView = {
+    let view = UIImageView()
+    view.image = UIImage(named: "surprised")
+    view.sizeToFit()
+    view.isUserInteractionEnabled = true
+    return view
+  }()
+  
+  private let tiredEmotion: UIImageView = {
+    let view = UIImageView()
+    view.image = UIImage(named: "tired")
+    view.sizeToFit()
+    view.isUserInteractionEnabled = true
+    return view
+  }()
+  
+  // MARK: - Life Cycle
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+  // MARK: - Functions
+  
+  override func addView() {
+    view.addSubview(containerView)
+    
+    [veryHappyEmotion, happyEmotion, lovelyEmotion,
+     surprisedEmotion, neutralEmotion, embarrassedEmotion,
+     hurtEmotion, sleepyEmotion, tiredEmotion,
+     angryEmotion, verySadEmotion, sadEmotion].forEach {
+      containerView.addSubview($0)
+    }
+  }
+  
+  override func setLayout() {
+    containerView.snp.makeConstraints { make in
+      make.edges.equalToSuperview()
+    }
+    
+    // 1 ROW
+    veryHappyEmotion.snp.makeConstraints { make in
+      make.top.equalToSuperview().offset(60)
+      make.width.height.equalTo(70)
+      make.centerX.equalTo(view.snp.leading).offset(view.bounds.width / 4.5)
+    }
+    
+    happyEmotion.snp.makeConstraints { make in
+      make.top.equalTo(veryHappyEmotion)
+      make.width.height.equalTo(70)
+      make.centerX.equalToSuperview()
+    }
+    
+    lovelyEmotion.snp.makeConstraints { make in
+      make.top.equalTo(veryHappyEmotion)
+      make.width.height.equalTo(70)
+      make.centerX.equalTo(view.snp.trailing).offset(-(view.bounds.width / 4.5))
+    }
+    
+    // 2 ROW
+    surprisedEmotion.snp.makeConstraints { make in
+      make.top.equalTo(veryHappyEmotion.snp.bottom).offset(20)
+      make.width.height.equalTo(70)
+      make.centerX.equalTo(veryHappyEmotion)
+    }
+    
+    neutralEmotion.snp.makeConstraints { make in
+      make.top.equalTo(surprisedEmotion)
+      make.width.height.equalTo(70)
+      make.centerX.equalTo(happyEmotion)
+    }
+    
+    embarrassedEmotion.snp.makeConstraints { make in
+      make.top.equalTo(surprisedEmotion)
+      make.width.height.equalTo(70)
+      make.centerX.equalTo(lovelyEmotion)
+    }
+    
+    // 3 ROW
+    hurtEmotion.snp.makeConstraints { make in
+      make.top.equalTo(surprisedEmotion.snp.bottom).offset(20)
+      make.width.height.equalTo(70)
+      make.centerX.equalTo(veryHappyEmotion)
+    }
+    
+    sleepyEmotion.snp.makeConstraints { make in
+      make.top.equalTo(hurtEmotion)
+      make.width.height.equalTo(70)
+      make.centerX.equalTo(happyEmotion)
+    }
+    
+    tiredEmotion.snp.makeConstraints { make in
+      make.top.equalTo(hurtEmotion)
+      make.width.height.equalTo(70)
+      make.centerX.equalTo(lovelyEmotion)
+    }
+    
+    // 4 ROW
+    angryEmotion.snp.makeConstraints { make in
+      make.top.equalTo(hurtEmotion.snp.bottom).offset(20)
+      make.width.height.equalTo(70)
+      make.centerX.equalTo(veryHappyEmotion)
+    }
+    
+    verySadEmotion.snp.makeConstraints { make in
+      make.top.equalTo(angryEmotion)
+      make.width.height.equalTo(70)
+      make.centerX.equalTo(happyEmotion)
+    }
+    
+    sadEmotion.snp.makeConstraints { make in
+      make.top.equalTo(angryEmotion)
+      make.width.height.equalTo(70)
+      make.centerX.equalTo(lovelyEmotion)
+      make.bottom.lessThanOrEqualTo(containerView.snp.bottom).offset(-20)
+    }
+  }
+  
+  override func setupView() {
+    view.backgroundColor = .azBlack
+    
+    if let sheet = sheetPresentationController {
+      sheet.detents = [.medium(), .large()]
+      sheet.preferredCornerRadius = 24
+    }
+    
+    addTapGestures()
+  }
+}
 
-//final class EmotionalImagePopupView: BaseView {
-//  
-//  // MARK: - Properties
-//  
-//  weak var delegate: EmotionalImagePopupViewDelegate?
-//  
-//  // MARK: - Views
-//  
-//  let dimmingView: UIView = {
-//    let view = UIView()
-//    return view
-//  }()
-//  
-//  private let emotionalImagePopupView: UIView = {
-//    let view = UIView()
-//    view.backgroundColor = .azWhite
-//    view.layer.cornerRadius = 16
-//    return view
-//  }()
-//  
-//  private let veryHappyEmotion: UIImageView = {
-//    let view = UIImageView()
-//    view.image = UIImage(named: "very_happy")
-//    view.sizeToFit()
-//    view.isUserInteractionEnabled = true
-//    return view
-//  }()
-//  
-//  private let happyEmotion: UIImageView = {
-//    let view = UIImageView()
-//    view.image = UIImage(named: "happy")
-//    view.sizeToFit()
-//    view.isUserInteractionEnabled = true
-//    return view
-//  }()
-//  
-//  private let neutralEmotion: UIImageView = {
-//    let view = UIImageView()
-//    view.image = UIImage(named: "neutral")
-//    view.sizeToFit()
-//    view.isUserInteractionEnabled = true
-//    return view
-//  }()
-//  
-//  private let verySadEmotion: UIImageView = {
-//    let view = UIImageView()
-//    view.image = UIImage(named: "very_sad")
-//    view.sizeToFit()
-//    view.isUserInteractionEnabled = true
-//    return view
-//  }()
-//  
-//  private let sadEmotion: UIImageView = {
-//    let view = UIImageView()
-//    view.image = UIImage(named: "sad")
-//    view.sizeToFit()
-//    view.isUserInteractionEnabled = true
-//    return view
-//  }()
-//  
-//  private let angryEmotion: UIImageView = {
-//    let view = UIImageView()
-//    view.image = UIImage(named: "angry")
-//    view.sizeToFit()
-//    view.isUserInteractionEnabled = true
-//    return view
-//  }()
-//  
-//  // MARK: - Init
-//  
-//  override init(frame: CGRect) {
-//    super.init(frame: frame)
-//  }
-//  
-//  required init?(coder: NSCoder) {
-//    super.init(coder: coder)
-//  }
-//  
-//  // MARK: - Functions
-//  
-//  override func addView() {
-//    [dimmingView, emotionalImagePopupView].forEach {
-//      addSubview($0)
-//    }
-//    
-//    [veryHappyEmotion, happyEmotion, neutralEmotion,
-//     verySadEmotion, sadEmotion, angryEmotion].forEach {
-//      emotionalImagePopupView.addSubview($0)
-//    }
-//  }
-//  
-//  override func setLayout() {
-//    dimmingView.snp.makeConstraints { make in
-//      make.edges.equalToSuperview()
-//    }
-//    
-//    veryHappyEmotion.snp.makeConstraints { make in
-//      make.top.equalToSuperview().offset(20)
-//      make.leading.equalToSuperview().offset(20)
-//      make.width.height.equalTo(60)
-//    }
-//    
-//    happyEmotion.snp.makeConstraints { make in
-//      make.top.equalToSuperview().offset(20)
-//      make.centerX.equalToSuperview()
-//      make.width.height.equalTo(60)
-//    }
-//    
-//    neutralEmotion.snp.makeConstraints { make in
-//      make.top.equalToSuperview().offset(20)
-//      make.trailing.equalToSuperview().offset(-20)
-//      make.width.height.equalTo(60)
-//    }
-//    
-//    verySadEmotion.snp.makeConstraints { make in
-//      make.top.equalTo(happyEmotion.snp.bottom).offset(20)
-//      make.leading.equalToSuperview().offset(20)
-//      make.width.height.equalTo(60)
-//    }
-//    
-//    sadEmotion.snp.makeConstraints { make in
-//      make.top.equalTo(happyEmotion.snp.bottom).offset(20)
-//      make.centerX.equalToSuperview()
-//      make.width.height.equalTo(60)
-//    }
-//    
-//    angryEmotion.snp.makeConstraints { make in
-//      make.top.equalTo(neutralEmotion.snp.bottom).offset(20)
-//      make.trailing.equalToSuperview().offset(-20)
-//      make.width.height.equalTo(60)
-//    }
-//    
-//    emotionalImagePopupView.snp.makeConstraints { make in
-//      make.center.equalToSuperview()
-//      make.width.equalTo(250)
-//      make.height.equalTo(180)
-//    }
-//  }
-//  
-//  override func setupView() {
-//    DispatchQueue.main.async { [weak self] in
-//      self?.dimmingView.alpha = 0
-//      self?.emotionalImagePopupView.alpha = 0
-//    }
-//    addTapGestures()
-//  }
-//}
-//
-//extension EmotionalImagePopupView {
-//  func showPopup() {
-//    DispatchQueue.main.async { [weak self] in
-//      UIView.animate(withDuration: 0.6) {
-//        self?.dimmingView.alpha = 1
-//        self?.emotionalImagePopupView.alpha = 1
-//      }
-//    }
-//  }
-//  
-//  func hidePopup(completion: @escaping () -> Void) {
-//    DispatchQueue.main.async { [weak self] in
-//      UIView.animate(withDuration: 0.3, animations: {
-//        self?.dimmingView.alpha = 0
-//        self?.emotionalImagePopupView.alpha = 0
-//      }) { _ in
-//        completion()
-//      }
-//    }
-//  }
-//  
-//  private func addTapGestures() {
-//    let veryHappyTapGesture = UITapGestureRecognizer(
-//      target: self,
-//      action: #selector(imageTapped(_:))
-//    )
-//    veryHappyEmotion.addGestureRecognizer(veryHappyTapGesture)
-//    veryHappyEmotion.tag = 1
-//    
-//    let happyTapGesture = UITapGestureRecognizer(
-//      target: self,
-//      action: #selector(imageTapped(_:))
-//    )
-//    happyEmotion.addGestureRecognizer(happyTapGesture)
-//    happyEmotion.tag = 2
-//    
-//    let neutralTapGesture = UITapGestureRecognizer(
-//      target: self,
-//      action: #selector(imageTapped(_:))
-//    )
-//    neutralEmotion.addGestureRecognizer(neutralTapGesture)
-//    neutralEmotion.tag = 3
-//    
-//    let verySadTapGesture = UITapGestureRecognizer(
-//      target: self,
-//      action: #selector(imageTapped(_:))
-//    )
-//    verySadEmotion.addGestureRecognizer(verySadTapGesture)
-//    verySadEmotion.tag = 4
-//    
-//    let sadTapGesture = UITapGestureRecognizer(
-//      target: self,
-//      action: #selector(imageTapped(_:))
-//    )
-//    sadEmotion.addGestureRecognizer(sadTapGesture)
-//    sadEmotion.tag = 5
-//    
-//    let angryTapGesture = UITapGestureRecognizer(
-//      target: self,
-//      action: #selector(imageTapped(_:))
-//    )
-//    angryEmotion.addGestureRecognizer(angryTapGesture)
-//    angryEmotion.tag = 6
-//  }
-//  
-//  @objc private func imageTapped(_ sender: UITapGestureRecognizer) {
-//    guard let tappedView = sender.view else { return }
-//    
-//    switch tappedView.tag {
-//    case 1:
-//      hidePopup {
-//        self.delegate?.emotionalImageTapTrigger(selectEmotionType: .very_happy)
-//      }
-//    case 2:
-//      hidePopup {
-//        self.delegate?.emotionalImageTapTrigger(selectEmotionType: .happy)
-//      }
-//    case 3:
-//      hidePopup {
-//        self.delegate?.emotionalImageTapTrigger(selectEmotionType: .neutral)
-//      }
-//    case 4:
-//      hidePopup {
-//        self.delegate?.emotionalImageTapTrigger(selectEmotionType: .very_sad)
-//      }
-//    case 5:
-//      hidePopup {
-//        self.delegate?.emotionalImageTapTrigger(selectEmotionType: .sad)
-//      }
-//    case 6:
-//      hidePopup {
-//        self.delegate?.emotionalImageTapTrigger(selectEmotionType: .angry)
-//      }
-//    default:
-//      break
-//    }
-//  }
-//}
+private extension EmotionalBottomSheetViewController {
+  func addTapGestures() {
+    let veryHappyTapGesture = UITapGestureRecognizer(
+      target: self,
+      action: #selector(imageTapped(_:))
+    )
+    veryHappyEmotion.addGestureRecognizer(veryHappyTapGesture)
+    veryHappyEmotion.tag = 1
+    
+    let happyTapGesture = UITapGestureRecognizer(
+      target: self,
+      action: #selector(imageTapped(_:))
+    )
+    happyEmotion.addGestureRecognizer(happyTapGesture)
+    happyEmotion.tag = 2
+    
+    let neutralTapGesture = UITapGestureRecognizer(
+      target: self,
+      action: #selector(imageTapped(_:))
+    )
+    neutralEmotion.addGestureRecognizer(neutralTapGesture)
+    neutralEmotion.tag = 3
+    
+    let verySadTapGesture = UITapGestureRecognizer(
+      target: self,
+      action: #selector(imageTapped(_:))
+    )
+    verySadEmotion.addGestureRecognizer(verySadTapGesture)
+    verySadEmotion.tag = 4
+    
+    let sadTapGesture = UITapGestureRecognizer(
+      target: self,
+      action: #selector(imageTapped(_:))
+    )
+    sadEmotion.addGestureRecognizer(sadTapGesture)
+    sadEmotion.tag = 5
+    
+    let angryTapGesture = UITapGestureRecognizer(
+      target: self,
+      action: #selector(imageTapped(_:))
+    )
+    angryEmotion.addGestureRecognizer(angryTapGesture)
+    angryEmotion.tag = 6
+    
+    let embarrassedTapGesture = UITapGestureRecognizer(
+      target: self,
+      action: #selector(imageTapped(_:))
+    )
+    embarrassedEmotion.addGestureRecognizer(embarrassedTapGesture)
+    embarrassedEmotion.tag = 7
+    
+    let hurtTapGesture = UITapGestureRecognizer(
+      target: self,
+      action: #selector(imageTapped(_:))
+    )
+    hurtEmotion.addGestureRecognizer(hurtTapGesture)
+    hurtEmotion.tag = 8
+    
+    let lovelyTapGesture = UITapGestureRecognizer(
+      target: self,
+      action: #selector(imageTapped(_:))
+    )
+    lovelyEmotion.addGestureRecognizer(lovelyTapGesture)
+    lovelyEmotion.tag = 9
+    
+    let sleepyTapGesture = UITapGestureRecognizer(
+      target: self,
+      action: #selector(imageTapped(_:))
+    )
+    sleepyEmotion.addGestureRecognizer(sleepyTapGesture)
+    sleepyEmotion.tag = 10
+    
+    let surprisedTapGesture = UITapGestureRecognizer(
+      target: self,
+      action: #selector(imageTapped(_:))
+    )
+    surprisedEmotion.addGestureRecognizer(surprisedTapGesture)
+    surprisedEmotion.tag = 11
+    
+    let tiredTapGesture = UITapGestureRecognizer(
+      target: self,
+      action: #selector(imageTapped(_:))
+    )
+    tiredEmotion.addGestureRecognizer(tiredTapGesture)
+    tiredEmotion.tag = 12
+  }
+  
+  @objc func imageTapped(_ sender: UITapGestureRecognizer) {
+    guard let tappedView = sender.view else { return }
+    
+    switch tappedView.tag {
+    case 1:
+      delegate?.emotionalImageTapTrigger(selectEmotionType: .very_happy)
+    case 2:
+      delegate?.emotionalImageTapTrigger(selectEmotionType: .happy)
+    case 3:
+      delegate?.emotionalImageTapTrigger(selectEmotionType: .neutral)
+    case 4:
+      delegate?.emotionalImageTapTrigger(selectEmotionType: .very_sad)
+    case 5:
+      delegate?.emotionalImageTapTrigger(selectEmotionType: .sad)
+    case 6:
+      delegate?.emotionalImageTapTrigger(selectEmotionType: .angry)
+    case 7:
+      delegate?.emotionalImageTapTrigger(selectEmotionType: .embarrassed)
+    case 8:
+      delegate?.emotionalImageTapTrigger(selectEmotionType: .hurt)
+    case 9:
+      delegate?.emotionalImageTapTrigger(selectEmotionType: .lovely)
+    case 10:
+      delegate?.emotionalImageTapTrigger(selectEmotionType: .sleepy)
+    case 11:
+      delegate?.emotionalImageTapTrigger(selectEmotionType: .surprised)
+    case 12:
+      delegate?.emotionalImageTapTrigger(selectEmotionType: .tired)
+    default:
+      break
+    }
+    dismiss(animated: true)
+  }
+}
