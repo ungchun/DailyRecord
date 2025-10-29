72c0f09592ab4251f45b8decebc691aa0dcee9eb
sunghun <leedool3003@gmail.com>
2025-10-29 23:59:34 +0900

    :sparkles: [feat] setup FirebaseCrashlytics

--- DailyRecord/Application/AppDelegate.swift
+++ DailyRecord/Application/AppDelegate.swift
@@ -10,33 +10,34 @@ import UIKit
 import AmplitudeSwift
 import Firebase
 import FirebaseAnalytics
+import FirebaseCrashlytics
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-	func application(
-		_ application: UIApplication,
-		didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-	) -> Bool {
-		FirebaseApp.configure()
-		Amp.configure(apiKey: "a94605679e0892a6387c7a7657c4a1c4")
-		return true
-	}
-	
-	// MARK: UISceneSession Lifecycle
-	
-	func application(
-		_ application: UIApplication,
-		configurationForConnecting connectingSceneSession: UISceneSession,
-		options: UIScene.ConnectionOptions
-	) -> UISceneConfiguration {
-		return UISceneConfiguration(name: "Default Configuration",
-																sessionRole: connectingSceneSession.role)
-	}
-	
-	func application(
-		_ application: UIApplication,
-		didDiscardSceneSessions sceneSessions: Set<UISceneSession>
-	) {
-		
-	}
+  func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    FirebaseApp.configure()
+    Amp.configure(apiKey: "a94605679e0892a6387c7a7657c4a1c4")
+    return true
+  }
+  
+  // MARK: UISceneSession Lifecycle
+  
+  func application(
+    _ application: UIApplication,
+    configurationForConnecting connectingSceneSession: UISceneSession,
+    options: UIScene.ConnectionOptions
+  ) -> UISceneConfiguration {
+    return UISceneConfiguration(name: "Default Configuration",
+                                sessionRole: connectingSceneSession.role)
+  }
+  
+  func application(
+    _ application: UIApplication,
+    didDiscardSceneSessions sceneSessions: Set<UISceneSession>
+  ) {
+    
+  }
 }
